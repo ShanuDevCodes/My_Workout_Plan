f3f0859ec85dbebc60b30e8419da42c7c1de0143
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-05-24 13:51:31 +0530

    v1

--- app/src/main/java/com/example/myworkoutplan/ui/PortraitUI.kt
+++ app/src/main/java/com/example/myworkoutplan/ui/PortraitUI.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
