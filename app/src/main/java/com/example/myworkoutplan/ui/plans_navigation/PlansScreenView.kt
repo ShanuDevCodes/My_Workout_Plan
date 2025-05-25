2d3a55ca63f4bfac51095d39e9ba4b9ef9701821
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-05-25 16:27:51 +0530

    v1

--- app/src/main/java/com/example/myworkoutplan/ui/plans_navigation/PlansScreenView.kt
+++ app/src/main/java/com/example/myworkoutplan/ui/plans_navigation/PlansScreenView.kt
@@ -1,5 +1,8 @@
 package com.example.myworkoutplan.ui.plans_navigation
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,13 +11,29 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
 import com.example.myworkoutplan.ui.screen.DayScreen
+import kotlinx.coroutines.delay
 
 @Composable
 fun PlansScreenView(dayTitle: String, workoutList: List<Pair<String, Int>>) {
+    val fabScale = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        delay(300)
+        fabScale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 100,
+                easing = FastOutSlowInEasing
+            )
+        )
+    }
     Box(modifier = Modifier
         .fillMaxSize()){
         DayScreen(
@@ -27,7 +46,8 @@ fun PlansScreenView(dayTitle: String, workoutList: List<Pair<String, Int>>) {
             },
             modifier = Modifier
                 .align(Alignment.BottomEnd)
-                .padding(16.dp)
+                .padding(8.dp)
+                .scale(fabScale.value)
         ) {
             Icon(
                 imageVector = Icons.Default.Edit,
