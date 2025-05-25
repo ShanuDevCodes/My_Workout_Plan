6ff88a1c8a0e74d9937e58bea0244d608ea733e5
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-05-26 03:22:44 +0530

    v1

--- app/src/main/java/com/example/myworkoutplan/ui/screen/DayScreen.kt
+++ app/src/main/java/com/example/myworkoutplan/ui/screen/DayScreen.kt
@@ -1,27 +1,16 @@
 package com.example.myworkoutplan.ui.screen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.example.myworkoutplan.R
 import com.example.myworkoutplan.ui.components.PlansCards
 
 @Composable
