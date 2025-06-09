525bfe8fc4622a9c8cdb1ebfc429fcd831901d5d
Kaneki.Uzumaki_ <shanu.dev.codes@gmail.com>
2025-06-10 02:41:31 +0530

    v3.0.10-beta

--- app/src/main/java/com/example/myworkoutplan/features/mainapp/ui/WorkoutListScreen.kt
+++ app/src/main/java/com/example/myworkoutplan/features/mainapp/ui/WorkoutListScreen.kt
@@ -1,6 +1,5 @@
 package com.example.myworkoutplan.features.mainapp.ui
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -18,29 +18,38 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myworkoutplan.core.AppDatabase
+import com.example.myworkoutplan.data.local.workout.WorkoutEvent
 import com.example.myworkoutplan.data.local.workout.WorkoutViewModel
 import com.example.myworkoutplan.data.local.workout.WorkoutViewModelFactory
-import com.example.myworkoutplan.features.mainapp.ui.workout.DayCards
-import com.example.myworkoutplan.features.mainapp.ui.workout.DayScreen
+import com.example.myworkoutplan.features.mainapp.ui.workout.PlansCards
+import com.example.myworkoutplan.features.mainapp.viewmodel.DayScreenViewModel
 import com.example.myworkoutplan.features.mainapp.viewmodel.WorkoutListScreenViewModel
 import kotlinx.coroutines.delay
+import androidx.compose.runtime.getValue
 
 @Composable
 fun WorkoutListScreen(workoutListScreenViewModel: WorkoutListScreenViewModel = viewModel()){
-    val visible = workoutListScreenViewModel.visible
     val context = LocalContext.current
     val db = remember { AppDatabase.getInstance(context)}
     val dao = remember { db.WorkoutDao() }
     val workoutViewModel: WorkoutViewModel = viewModel(
         factory = WorkoutViewModelFactory(dao)
     )
+    val workoutState by workoutViewModel.state.collectAsState()
+    val dayScreenViewModel: DayScreenViewModel = viewModel()
+    dayScreenViewModel.setDayTitle("All")
+    val muscleGroups = dayScreenViewModel.muscleGroups
+    LaunchedEffect(muscleGroups) {
+        workoutViewModel.onEvent(WorkoutEvent.GetWorkoutsByMuscleGroup(muscleGroups))
+    }
     LaunchedEffect(Unit) {
         delay(300)
         workoutListScreenViewModel.show()
     }
-    Column {
+    LazyColumn {
+        item {
             Text(
-                text = "All Workout List",
+                text = "All Workouts List",
                 style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
                 color = MaterialTheme.colorScheme.primary,
                 modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)
@@ -49,11 +58,11 @@ fun WorkoutListScreen(workoutListScreenViewModel: WorkoutListScreenViewModel = v
             HorizontalDivider(
                 thickness = 1.dp,
                 color = MaterialTheme.colorScheme.outlineVariant.copy(alpha = 0.4f),
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
             )
-
-
-            DayScreen(visible = visible, dayTitle = "All", workoutViewModel = workoutViewModel)
-
+        }
+        items(workoutState.workoutWithMuscleGroups) { item ->
+            PlansCards(item)
+        }
     }
 }
